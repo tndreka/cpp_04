7dcdf5a0a40b349906701fc81ed14ea73a9646b0
tndreka <tndreka@student.42heilbronn.de>
2025-07-16 18:14:44 +0200

    a

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 17:59:47 by tndreka           #+#    #+#             */
-/*   Updated: 2025/07/16 18:05:30 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/07/16 18:13:05 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ public:
 	Brain(const Brain& other);
 	Brain& operator=(const Brain& other);
 	~Brain();
+	//
 };
 
 #endif
