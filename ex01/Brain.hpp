bbc3d82c735fb2021877c438e2aeea8a706bcbe9
tndreka <tndreka@student.42heilbronn.de>
2025-07-16 18:10:24 +0200

    ADD: started implementin and initing the Brain Class

---
+++ ex01/Brain.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 17:59:47 by tndreka           #+#    #+#             */
+/*   Updated: 2025/07/16 18:05:30 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+#include <string>
+
+class Brain
+{
+protected:
+	std::string ideas[100];
+public:
+	Brain();
+	Brain(const Brain& other);
+	Brain& operator=(const Brain& other);
+	~Brain();
+};
+
+#endif
